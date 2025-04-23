985aa37cd28bf0681ac6bcbe59f04e3f9407e351
y3sterd4y <aninaa@fcpn.edu.bo>
2025-04-22 20:41:04 -0400

    Header repair

---
+++ frontend/src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className='hero is-primary is-small'>
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <h1 className="title">Lista de Canciones</h1>
+          <h2 className="subtitle">Una coleccion de canciones mis favoritas</h2>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,12 @@
 import 'bulma/css/bulma.min.css';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Navbar from './components/Navbar/'
+import Header from './components/Header'
 import Table from './components/Table/'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Navbar/>
+    <Header/>
     <Table/>
   </StrictMode>,
 )
