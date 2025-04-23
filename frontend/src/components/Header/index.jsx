bc47d1eb522cc3ec9be1e1c236296ea4cb6cc86f
ALEX GABRIEL NINA ALIAGA <aninaa@fcpn.edu.bo>
2025-04-23 10:51:25 +0000

    CodeSpace Test

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -5,7 +5,7 @@ function Header() {
     <header className='hero is-primary is-small'>
       <div className="hero-body">
         <div className="container has-text-centered">
-          <h1 className="title">Lista de Canciones</h1>
+          <h1 className="title">List of songs</h1>
           <h2 className="subtitle">Una coleccion de mis canciones favoritas</h2>
         </div>
       </div>
